e3db6f44b82f6320ef06555d7435174d6ca98830
archiechop1 <121496204+Ringolall@users.noreply.github.com>
2023-01-20 16:07:16 +0200

    add click funtions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-console.log("tere tere, vana kere");
-
 jQuery(document).ready(function() {
 
     jQuery("p").css("color", "blue");
+
+    jQuery(".burger-menu").click(function() {
+        console.log("tere tere, vana kere");
+    });
+    jQuery(".menu__dropdown").parent(".menu__list-item").click(function() {
+        console.log("tere rippmenü");
+        jQuery(this).children(".menu__dropdown").slideToggle();
+    });
+    jQuery(".back-to-the-top").click(function(event) {
+        event.preventDefault();
+        console.log("ei tea");
+    });
+
+    jQuery(window).scroll(function(event) {
+        console.log("tere tere, vana kere");
+
+    });
 
 });

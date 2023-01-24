3c9ad4292c03ae67478df259f34a07842fae9fab
archiechop1 <121496204+Ringolall@users.noreply.github.com>
2023-01-24 14:50:01 +0200

    fixed dropdown menu, modified header scroll, add scroll,

--- js/script.js
+++ js/script.js
@@ -1,21 +1,55 @@
 jQuery(document).ready(function() {
 
-    jQuery("p").css("color", "blue");
+    // kas burgermenüüd klikitakse?
 
     jQuery(".burger-menu").click(function() {
-        console.log("tere tere, vana kere");
-    });
-    jQuery(".menu__dropdown").parent(".menu__list-item").click(function() {
-        console.log("tere rippmenü");
-        jQuery(this).children(".menu__dropdown").slideToggle();
-    });
-    jQuery(".back-to-the-top").click(function(event) {
-        event.preventDefault();
-        console.log("ei tea");
+        jQuery(this).siblings(".fixed-sidebar").addClass("fixed-sidebar--active")
+
+
     });
 
+    jQuery(".close-button").click(function() {
+
+        jQuery(this).closest(".fixed-sidebar--active").removeClass("fixed-sidebar--active")
+    })
+
+    // menu dropdown parentit .menu__list-item klikitakse?
+    jQuery(".menu__has-sub a").click(function(event) {
+        event.preventDefault();
+        console.log("tere rippmenü");
+        //see element kui klikitakse siis kuva selle menu__dropdown nimelised alamelemendid
+        jQuery(this).siblings(".menu__dropdown").stop().slideToggle();
+    });
+
+
+    jQuery(".back-to-the-top").click(function(event) {
+        // ära tee default tegevust
+        event.preventDefault();
+        jQuery("html, body").animate({ scrollTop: 0 }, "fast");
+    });
+
+
+    var kuikorgel;
+    var scrolltarget = jQuery("#scrollto").offset().top;
+
+    console.log(scrolltarget);
+
     jQuery(window).scroll(function(event) {
-        console.log("tere tere, vana kere");
+        //kui akent keritakse siis näitame consolis
+
+        if (jQuery(window).width() > 800) {
+            kuikorgel = jQuery(window).scrollTop();
+            console.log(kuikorgel);
+            if (kuikorgel > scrolltarget) {
+                jQuery(".header--fixed").slideDown("slow");
+            } else {
+                jQuery(".header--fixed").slideUp("fast");
+
+            }
+
+        }
+
+
 
     });
 
